9acb5b4c9458adb225357ce7c57cf6f328ef389d
shyamtala003 <shyamtala003@gmail.com>
2023-03-21 15:53:16 +0530

    razorpay controller added

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -38,3 +38,42 @@ exports.captureStripePayment = async (req, res) => {
         })
     }
 }
+
+exports.sendStripeKey = (req, res) => {
+    try {
+        res.status(200).json({
+            razorpaykey: process.env.RAZORPAY_API_KEY,
+        });
+    } catch (error) {
+        res.status(500).json({
+            succss: false,
+            message: error.message
+        });
+    }
+}
+
+exports.exports.captureRazorpayPayment = async (req, res) => {
+    try {
+        let instance = new Razorpay({
+            key_id: process.env.RAZORPAY_API_KEY,
+            key_secret: process.env.RAZORPAY_SECRET,
+        });
+
+        let options = {
+            amount: req.body.amount * 100, // amount in the smallest currency unit (paisa)
+            currency: "INR",
+        };
+        const myOrder = await instance.orders.create(options);
+
+        res.status(200).json({
+            success: true,
+            amount: req.body.amount,
+            order: myOrder,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message
+        })
+    }
+}
