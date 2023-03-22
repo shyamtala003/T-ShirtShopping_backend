7be02b687fe83c4a11f38add6dcb4968925d6e0a
shyamtala003 <shyamtala003@gmail.com>
2023-03-22 10:38:20 +0530

    routes added for fetching order for user and admin

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -34,6 +34,43 @@ exports.createOrder=async(req,res)=>{
 exports.getOneOrder=async (req, res)=>{
     try {
         const order = await Order.findById(req.params.id).populate('user',"name email");
+        
+        if(!order){
+            return res.status(404).json({success:false,error:"please check order id"});
+        }
+        res.status(200).json({
+            success: true,
+            order
+        });
+    } catch (error) {
+        res.status(403).json({success:false,error: error.message});
+    }
+}
+
+exports.getLoggedinOrder=async (req, res)=>{
+    try {
+        const order = await Order.find({user:req.user._id});
+        
+        if(!order){
+            return res.status(404).json({success:false,error:"please check order id"});
+        }
+        res.status(200).json({
+            success: true,
+            order
+        });
+    } catch (error) {
+        res.status(403).json({success:false,error: error.message});
+    }
+}
+
+// admin route
+exports.adminGetOrder=async (req, res)=>{
+    try {
+        const order = await Order.find();
+        
+        if(!order){
+            return res.status(404).json({success:false,error:"please check order id"});
+        }
         res.status(200).json({
             success: true,
             order
--- routes/order.js
+++ routes/order.js
@@ -8,9 +8,14 @@ const {
 } = require("../middlewares/userValidator");
 
 // import controller
-const { createOrder,getOneOrder } = require("../controllers/orderController");
+const { createOrder,getOneOrder,getLoggedinOrder,adminGetOrder } = require("../controllers/orderController");
 
 router.route('/order/create').post(loggedInUser,createOrder)
 router.route('/order/:id').get(loggedInUser,getOneOrder)
+router.route('/myorder').get(loggedInUser,getLoggedinOrder)
+
+
+// admin route set up
+router.route('/admin/order').get(loggedInUser,customeRoles("admin"),adminGetOrder);
 
 module.exports = router;
