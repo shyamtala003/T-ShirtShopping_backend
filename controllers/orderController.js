fe7260610b7eae80022a609031e2022b6280d37d
shyamtala003 <shyamtala003@gmail.com>
2023-03-22 11:21:57 +0530

    admin delete product route added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -108,7 +108,10 @@ exports.adminUpdateOrder = async (req, res) => {
         const order = await Order.findById(req.params.id);
 
         if (order.orderStatus === "Delivered") {
-            return res.status(401).json({success:false, error:"Order is already marked for delivered"});
+            return res.status(401).json({
+                success: false,
+                error: "Order is already marked for delivered"
+            });
         }
 
         order.orderStatus = req.body.orderStatus;
@@ -134,9 +137,32 @@ exports.adminUpdateOrder = async (req, res) => {
 // function for updating a product stock
 async function updateProductStock(productId, quantity) {
     const product = await Product.findById(productId);
-  
+
     product.stock = product.stock - quantity;
-  
-    await product.save({ validateBeforeSave: false });
-  }
-  
+
+    await product.save({
+        validateBeforeSave: false
+    });
+}
+
+
+exports.adminDeleteOrder = async (req, res) => {
+    try {
+
+        const order = await Order.findById(req.params.id);
+        if(!order)
+        {
+            return res.status(500).json({success: false, message:"order not found"})
+        }
+        await Order.findByIdAndRemove(req.params.id);
+        
+        res.status(200).json({
+            success: true,order
+        });
+    } catch (error) {
+        res.status(403).json({
+            success: false,
+            error: error.message
+        });
+    }
+}
--- routes/order.js
+++ routes/order.js
@@ -8,7 +8,7 @@ const {
 } = require("../middlewares/userValidator");
 
 // import controller
-const { createOrder,getOneOrder,getLoggedinOrder,adminGetOrder,adminUpdateOrder } = require("../controllers/orderController");
+const { createOrder,getOneOrder,getLoggedinOrder,adminGetOrder,adminUpdateOrder,adminDeleteOrder } = require("../controllers/orderController");
 
 router.route('/order/create').post(loggedInUser,createOrder)
 router.route('/order/:id').get(loggedInUser,getOneOrder)
@@ -18,6 +18,7 @@ router.route('/myorder').get(loggedInUser,getLoggedinOrder)
 // admin route set up
 router.route('/admin/order').get(loggedInUser,customeRoles("admin"),adminGetOrder);
 router.route('/admin/order/:id').put(loggedInUser,customeRoles("admin"),adminUpdateOrder);
+router.route('/admin/order/:id').delete(loggedInUser,customeRoles("admin"),adminDeleteOrder);
 
 
 module.exports = router;
